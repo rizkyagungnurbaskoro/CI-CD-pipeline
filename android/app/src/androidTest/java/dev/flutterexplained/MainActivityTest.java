3a04d34082928f5f085f7efc54c716748e0bc46a
rizkyagungnurbaskoro <rizky.nurbaskoro@gmail.com>
2024-06-09 11:40:49 +0800

    changes

--- android/app/src/androidTest/java/dev/flutterexplained/MainActivityTest.java
+++ android/app/src/androidTest/java/dev/flutterexplained/MainActivityTest.java
@@ -3,10 +3,7 @@ package dev.flutterexplained;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.junit.Assert.*;
-
 import com.example.flutter_application_1.MainActivity;
 
 @RunWith(AndroidJUnit4.class)
@@ -15,12 +12,5 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
 
-    @Test
-    public void testExample() {
-        // Add your test code here
-        // For example, you might check if a view is displayed
-        rule.getScenario().onActivity(activity -> {
-            assertNotNull(activity.findViewById(R.id.some_view));
-        });
-    }
+    // Add your test methods here
 }
